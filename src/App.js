3c531128fbc151b4482fc35bf94c5c4cef64a735
Amend Dev <amenddev@Mac-mini-de-Amend.local>
2020-01-30 16:30:09 -0300

    Implementação: Pagina Sign

--- src/App.js
+++ src/App.js
@@ -4,15 +4,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Sign from './pages/sign/sign.component'
 import Header from './components/header/header.component'
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
-
-
 
 function App() {
   return (
@@ -22,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
+          <Route path='/sign' component={Sign}/>
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/pages/sign/sign.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './sign.styles.scss'
+import SignIn from '../../components/sign-in/sign-in.component'
+
+const Sign = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn/>
+    </div>
+)
+
+export default Sign
