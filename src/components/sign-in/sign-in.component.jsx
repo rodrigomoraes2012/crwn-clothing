4772024f00ee31731d22fd8720c5d8ab85287b45
Amend Dev <amenddev@Mac-mini-de-Amend.local>
2020-01-30 17:32:29 -0300

    Implementação: Página Sing in, botão submit

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 
 import './sign-in.styles.scss'
 
@@ -47,7 +48,7 @@ class SignIn extends React.Component{
                         label='Password'
                         required />
                     
-                    <input type='submit' value='Submit Form' onChange={this.handleSubmit}></input>
+                    <CustomButton type='submit'>Sign In</CustomButton>
                 </form>
             </div>
         )
