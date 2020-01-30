8c916b7c9333cdbf3d4b9cd93507e8919bec2623
Amend Dev <amenddev@Mac-mini-de-Amend.local>
2020-01-30 15:44:30 -0300

    Implementação: Componente Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 const HatsPage = () => (
   <div>
@@ -15,6 +17,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div> 
+      <Header/>
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={HomePage}/>
