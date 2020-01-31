88750c6ad10eff23e4eb7b86d2895ed8d763390f
Amend Dev <amenddev@Mac-mini-de-Amend.local>
2020-01-31 11:08:13 -0300

    Implementação: Página Sing in e Sign out com Firebase

--- src/App.js
+++ src/App.js
@@ -6,20 +6,46 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Sign from './pages/sign/sign.component'
 import Header from './components/header/header.component'
+import {auth} from './firebase/firebase.utils'
 
-function App() {
-  return (
-    <div> 
-      <Header/>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}/>
-          <Route path='/sign' component={Sign}/>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(){
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth()
+  }
+
+  render(){
+    return (
+
+      <div> 
+        <Header currentUser={this.state.currentUser}/>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route path='/shop' component={ShopPage}/>
+            <Route path='/sign' component={Sign}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,9 @@ import {BrowserRouter, Link} from 'react-router-dom'
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import './header.styles.scss'
 
-const Header = () =>(
+import {auth} from '../../firebase/firebase.utils'
+
+const Header = ({currentUser}) =>(
     <div className='header'>
         <BrowserRouter>
             <Link className='logo-container' to='/'>
@@ -13,6 +15,14 @@ const Header = () =>(
             <div className='options'>
                 <Link className='option' to='/shop'>SHOP</Link>
                 <Link className='option' to='/contact'>CONTACT</Link>
+
+                {
+                    currentUser ?
+                    (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
+                    :
+                    (<Link className='option' to='/SIGN'>SIGN IN</Link>)
+                }
+
             </div>
         </BrowserRouter>
     </div>
